089bc02445e5ab7fd98442e06e1953b1d9893c49
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2021-04-26 13:34:55 -0300

    Fix AlunoDAO and TurmaDAO

--- src/br/edu/insper/desagil/backend/model/db/AlunoDAO.java
+++ src/br/edu/insper/desagil/backend/model/db/AlunoDAO.java
@@ -1,10 +1,11 @@
 package br.edu.insper.desagil.backend.model.db;
 
 import br.edu.insper.desagil.backend.core.FirestoreDAO;
+import br.edu.insper.desagil.backend.core.exception.APIException;
 import br.edu.insper.desagil.backend.model.Aluno;
 
 public class AlunoDAO extends FirestoreDAO<Aluno> {
-	public AlunoDAO() {
+	public AlunoDAO() throws APIException {
 		super("alunos");
 	}
 }
