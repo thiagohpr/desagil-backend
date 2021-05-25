a23d598d0ba8b03b1b5a912f53d0089d0ad6c75d
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2021-05-25 15:20:14 -0300

    Add autokey support

---
+++ src/br/edu/insper/desagil/backend/core/firestore/FirestoreAutokeyEntity.java
@@ -0,0 +1,6 @@
+package br.edu.insper.desagil.backend.core.firestore;
+
+import br.edu.insper.desagil.backend.core.AutokeyEntity;
+
+public abstract class FirestoreAutokeyEntity extends AutokeyEntity<String> {
+}
