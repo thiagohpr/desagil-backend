6c076e5d3e781d26dae0e582a398654c8094de31
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2021-05-25 15:20:14 -0300

    Add autokey support

--- src/br/edu/insper/desagil/backend/core/firestore/FirestoreEntity.java
+++ src/br/edu/insper/desagil/backend/core/firestore/FirestoreEntity.java
@@ -3,6 +3,4 @@ package br.edu.insper.desagil.backend.core.firestore;
 import br.edu.insper.desagil.backend.core.Entity;
 
 public abstract class FirestoreEntity implements Entity<String> {
-	@Override
-	public abstract String key();
 }
