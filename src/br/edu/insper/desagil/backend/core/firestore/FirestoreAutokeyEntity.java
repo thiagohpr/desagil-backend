d323583c371368da6c2ee5bc04b2b3228a3957c2
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2021-05-25 15:42:16 -0300

    Simplify autokey support

--- src/br/edu/insper/desagil/backend/core/firestore/FirestoreAutokeyEntity.java
+++ src/br/edu/insper/desagil/backend/core/firestore/FirestoreAutokeyEntity.java
@@ -1,6 +1,18 @@
 package br.edu.insper.desagil.backend.core.firestore;
 
-import br.edu.insper.desagil.backend.core.AutokeyEntity;
+public abstract class FirestoreAutokeyEntity extends FirestoreEntity {
+	private String key;
 
-public abstract class FirestoreAutokeyEntity extends AutokeyEntity<String> {
+	public final String getKey() {
+		return key;
+	}
+
+	public final void setKey(String key) {
+		this.key = key;
+	}
+
+	@Override
+	public final String key() {
+		return getKey();
+	}
 }
