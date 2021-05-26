f939a0e877560fc19a90e92455de83aa79bb7469
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2021-05-26 00:09:09 -0300

    Revert datum to interface

--- src/br/edu/insper/desagil/backend/core/Datum.java
+++ src/br/edu/insper/desagil/backend/core/Datum.java
@@ -1,5 +1,5 @@
 package br.edu.insper.desagil.backend.core;
 
-public abstract class Datum<K> {
-	public abstract K key();
+public interface Datum<K> {
+	K key();
 }
--- src/br/edu/insper/desagil/backend/core/firestore/FirestoreDatum.java
+++ src/br/edu/insper/desagil/backend/core/firestore/FirestoreDatum.java
@@ -2,5 +2,5 @@ package br.edu.insper.desagil.backend.core.firestore;
 
 import br.edu.insper.desagil.backend.core.Datum;
 
-public abstract class FirestoreDatum extends Datum<String> {
+public abstract class FirestoreDatum implements Datum<String> {
 }
