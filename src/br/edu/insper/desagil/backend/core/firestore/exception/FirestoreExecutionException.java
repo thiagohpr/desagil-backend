200ed7bde628cc2250222efa50a14c0456d70f88
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2021-05-26 00:09:33 -0300

    Update serials

--- src/br/edu/insper/desagil/backend/core/firestore/exception/FirestoreExecutionException.java
+++ src/br/edu/insper/desagil/backend/core/firestore/exception/FirestoreExecutionException.java
@@ -5,7 +5,7 @@ import java.util.concurrent.ExecutionException;
 import br.edu.insper.desagil.backend.core.exception.DBException;
 
 public class FirestoreExecutionException extends DBException {
-	private static final long serialVersionUID = 3825564659925631367L;
+	private static final long serialVersionUID = -5735895412390449890L;
 
 	public FirestoreExecutionException(ExecutionException exception) {
 		super("Firestore execution failed", exception);
--- src/br/edu/insper/desagil/backend/core/firestore/exception/FirestoreInterruptedException.java
+++ src/br/edu/insper/desagil/backend/core/firestore/exception/FirestoreInterruptedException.java
@@ -3,7 +3,7 @@ package br.edu.insper.desagil.backend.core.firestore.exception;
 import br.edu.insper.desagil.backend.core.exception.DBException;
 
 public class FirestoreInterruptedException extends DBException {
-	private static final long serialVersionUID = -5697905500804413537L;
+	private static final long serialVersionUID = 5081675235612820837L;
 
 	public FirestoreInterruptedException(InterruptedException exception) {
 		super("Firestore execution interrupted", exception);
