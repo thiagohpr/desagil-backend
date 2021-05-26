b1abaa3be89ba5158695dd191677e43b8eab3d38
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2021-05-26 19:25:10 -0300

    Fix fake daos

--- test/br/edu/insper/desagil/backend/model/db/FakeAlunoDAO.java
+++ test/br/edu/insper/desagil/backend/model/db/FakeAlunoDAO.java
@@ -1,12 +1,11 @@
 package br.edu.insper.desagil.backend.model.db;
 
 import br.edu.insper.desagil.backend.core.exception.APIException;
-import br.edu.insper.desagil.backend.core.exception.DBException;
 import br.edu.insper.desagil.backend.core.firestore.FirestoreDAO;
 import br.edu.insper.desagil.backend.model.Aluno;
 
 public class FakeAlunoDAO extends FirestoreDAO<Aluno> {
-	public FakeAlunoDAO() throws DBException, APIException {
+	public FakeAlunoDAO() throws APIException {
 		super("fake_alunos");
 	}
 }
